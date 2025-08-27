656422c864a55c325dfcfe1d38b0d4e0afa9938c
Patin Muangjan <github.pueblo862@slmails.com>
2025-08-27 22:59:25 +0700

    Sync progess with Git

--- resources/trananim.js
+++ resources/trananim.js
@@ -32,7 +32,7 @@ async function fetchPage(url){
 }
 
 async function transitionTo(path, push = true) {
-  const url = routes[path] || routes['#/'];
+  const url = routes[path] || routes['#/explore'];
   app.setAttribute('aria-busy', 'true');
   try {
     const html = await fetchPage(url);
